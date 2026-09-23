84842ba3fcd9c6085d54962a5f2f102bf6feef0f
Lucy Kaneva <lucy.kaneva07@gmail.com>
2026-09-22 21:46:42 -0400

    Merge upstream/main into sign-in-authentication

---
+++ src/config_sample.js
@@ -0,0 +1,17 @@
+// The demo version doesn't log to Firebase, and speeds up the Admin page by only downloading a small amount of sample data
+export const IS_DEMO_VERSION = true;
+
+// This is the password to log into the admin page and download the data
+export const adminPassword = "it's so so secret";
+
+// This is the Firebase configuration, which you can get from the Firebase console
+// (Order doesn't matter)
+export const firebaseConfig = {
+	apiKey: "alphanumeric",
+  authDomain: "yourproject.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "yourproject.firebasestorage.app",
+ 	messagingSenderId: "numeric",
+ 	appId: "numeric:numeric:numeric",
+ 	measurementId: "G-alphanumeric"
+};
